919cd5237ad93235872d8d6f79897878eb970825
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-08-25 17:59:06 +0530

    added refractor code

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.get("/webhook", (req, res) => {
   // Check if a token and mode were sent
   if (mode && token) {
     // Check the mode and token sent are correct
-    if (mode === "subscribe") {
+    if (mode === "subscribe") { // token === verify_token
       // Respond with 200 OK and challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
