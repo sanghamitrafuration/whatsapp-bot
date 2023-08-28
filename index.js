2fae519cc0851658edc4668efa234400abaddf10
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-08-28 19:13:30 +0530

    added list reply

--- index.js
+++ index.js
@@ -260,14 +260,12 @@ const buttonId1Response= (phone_number_id, from) => {
               "title": "SECTION_1_TITLE",
               "rows": [
                 {
-                  "id": "SECTION_1_ROW_1_ID",
-                  "title": "SECTION_1_ROW_1_TITLE",
-                  "description": "SECTION_1_ROW_1_DESCRIPTION"
+                  "id": "OUR_SERVICE_1_ID",
+                  "title": "Mobile App Development"
                 },
                 {
-                  "id": "SECTION_1_ROW_2_ID",
-                  "title": "SECTION_1_ROW_2_TITLE",
-                  "description": "SECTION_1_ROW_2_DESCRIPTION"
+                  "id": "OUR_SERVICE_2_ID",
+                  "title": "Web App Development"
                 }
               ]
             },
@@ -275,14 +273,12 @@ const buttonId1Response= (phone_number_id, from) => {
               "title": "SECTION_2_TITLE",
               "rows": [
                 {
-                  "id": "SECTION_2_ROW_1_ID",
-                  "title": "SECTION_2_ROW_1_TITLE",
-                  "description": "SECTION_2_ROW_1_DESCRIPTION"
+                  "id": "OUR_SERVICE_3_ID",
+                  "title": "Website Design & Development"
                 },
                 {
-                  "id": "SECTION_2_ROW_2_ID",
-                  "title": "SECTION_2_ROW_2_TITLE",
-                  "description": "SECTION_2_ROW_2_DESCRIPTION"
+                  "id": "OUR_SERVICE_4_ID",
+                  "title": "UI & UX Design"
                 }
               ]
             }
