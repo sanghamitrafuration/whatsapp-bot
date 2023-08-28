1207fc3603aff626a2ac1efc10d42ded06049dd4
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-08-28 19:10:02 +0530

    added list reply

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ const buttonId1Response= (phone_number_id, from) => {
     data: {
       "messaging_product": "whatsapp",
       "recipient_type": "individual",
-      "to": from,
+      "to": "PHONE_NUMBER",
       "type": "interactive",
       "interactive": {
         "type": "list",
@@ -260,12 +260,14 @@ const buttonId1Response= (phone_number_id, from) => {
               "title": "SECTION_1_TITLE",
               "rows": [
                 {
-                  "id": "OUR_SERVICE_1_ID",
-                  "title": "Mobile App Development"
+                  "id": "SECTION_1_ROW_1_ID",
+                  "title": "SECTION_1_ROW_1_TITLE",
+                  "description": "SECTION_1_ROW_1_DESCRIPTION"
                 },
                 {
-                  "id": "OUR_SERVICE_2_ID",
-                  "title": "Web App Development"
+                  "id": "SECTION_1_ROW_2_ID",
+                  "title": "SECTION_1_ROW_2_TITLE",
+                  "description": "SECTION_1_ROW_2_DESCRIPTION"
                 }
               ]
             },
@@ -273,112 +275,17 @@ const buttonId1Response= (phone_number_id, from) => {
               "title": "SECTION_2_TITLE",
               "rows": [
                 {
-                  "id": "OUR_SERVICE_3_ID",
-                  "title": "Website Design & Development"
-                },
-                {
-                  "id": "OUR_SERVICE_4_ID",
-                  "title": "UI & UX Design"
-                }
-              ]
-            },
-            {
-              "title": "SECTION_2_TITLE",
-              "rows": [
-                {
-                  "id": "OUR_SERVICE_5_ID",
-                  "title": "Custom Software Development"
-                },
-                {
-                  "id": "OUR_SERVICE_6_ID",
-                  "title": "Team Augmentation"
-                }
-              ]
-            },
-            {
-              "title": "SECTION_2_TITLE",
-              "rows": [
-                {
-                  "id": "OUR_SERVICE_7_ID",
+                  "id": "SECTION_2_ROW_1_ID",
                   "title": "SECTION_2_ROW_1_TITLE",
+                  "description": "SECTION_2_ROW_1_DESCRIPTION"
+                },
+                {
+                  "id": "SECTION_2_ROW_2_ID",
+                  "title": "SECTION_2_ROW_2_TITLE",
+                  "description": "SECTION_2_ROW_2_DESCRIPTION"
                 }
               ]
             }
-            // {
-            //   "title": "Our service",
-            //   "rows": [
-            //     {
-            //       "id": "OUR_SERVICE_1_ID",
-            //       "title": "Mobile App Development",
-            //       "description": "Mobile App Development"
-            //     },
-            //     {
-            //       "id": "OUR_SERVICE_2_ID",
-            //       "title": "SECTION_1_ROW_2_TITLE",
-            //       "description": "SECTION_1_ROW_2_DESCRIPTION"
-            //     },
-            //     {
-            //       "id": "OUR_SERVICE_2_ID",
-            //       "title": "Website Design & Development",
-            //       "description": "Website Design & Development"
-            //     },
-            //     {
-            //       "id": "OUR_SERVICE__ID",
-            //       "title": "UI & UX Design",
-            //       "description": "UI & UX Design"
-            //     },
-            //     {
-            //       "id": "OUR_SERVICE_5_ID",
-            //       "title": "Custom Software Development",
-            //       "description": "Custom Software Development"
-            //     },
-            //     {
-            //       "id": "OUR_SERVICE_6_ID",
-            //       "title": "Team Augmentation",
-            //       "description": "Team Augmentation"
-            //     },
-            //     {
-            //       "id": "OUR_SERVICE_7_ID",
-            //       "title": "Design & Product Strategy",
-            //       "description": "Design & Product Strategy"
-            //     }
-            //     // {
-            //     //   "id": "OUR_SERVICE_1_ID",
-            //     //   "title": "Mobile App Development",
-            //     //   "description": "Mobile App Development"
-            //     // },
-            //     // {
-            //     //   "id": "OUR_SERVICE_2_ID",
-            //     //   "title": "Web App Development",
-            //     //   "description": "Web App Development"
-            //     // },
-            //     // {
-            //     //   "id": "OUR_SERVICE_3_ID",
-            //     //   "title": "Website Design & Development",
-            //     //   "description": "Website Design & Development"
-            //     // },
-            //     // {
-            //     //   "id": "OUR_SERVICE_4_ID",
-            //     //   "title": "UI & UX Design",
-            //     //   "description": "UI & UX Design"
-            //     // },
-            //     // {
-            //     //   "id": "OUR_SERVICE_5_ID",
-            //     //   "title": "Custom Software Development",
-            //     //   "description": "Custom Software Development"
-            //     // },
-            //     // {
-            //     //   "id": "OUR_SERVICE_6_ID",
-            //     //   "title": "Team Augmentation",
-            //     //   "description": "Team Augmentation"
-            //     // },
-            //     // {
-            //     //   "id": "OUR_SERVICE_7_ID",
-            //     //   "title": "Design & Product Strategy",
-            //     //   "description": "Design & Product Strategy"
-            //     // }
-            //   ]
-            // }
           ]
         }
       }
