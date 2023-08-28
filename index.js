a2a500be3da090d3d1b0c45d080c63b7aa6a6325
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-08-28 18:32:29 +0530

    added list reply

--- index.js
+++ index.js
@@ -295,6 +295,21 @@ const buttonId1Response= (phone_number_id, from) => {
                   "description": "Design & Product Strategy"
                 }
               ]
+            },
+            {
+              "title": "SECTION_2_TITLE",
+              "rows": [
+                {
+                  "id": "SECTION_2_ROW_1_ID",
+                  "title": "SECTION_2_ROW_1_TITLE",
+                  "description": "SECTION_2_ROW_1_DESCRIPTION"
+                },
+                {
+                  "id": "SECTION_2_ROW_2_ID",
+                  "title": "SECTION_2_ROW_2_TITLE",
+                  "description": "SECTION_2_ROW_2_DESCRIPTION"
+                }
+              ]
             }
           ]
         }
