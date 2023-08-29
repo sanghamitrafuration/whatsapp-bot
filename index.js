c9058c38f6d6f5685ac990ce3e781cdbbca19ab5
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-08-29 11:55:26 +0530

    added list reply

--- index.js
+++ index.js
@@ -275,11 +275,11 @@ const buttonId1Response= (phone_number_id, from) => {
               "rows": [
                 {
                   "id": "OUR_SERVICE_3_ID",
-                  "title": "SECTION_2_ROW_1_TITLE"
+                  "title": "Website Design & Development"
                 },
                 {
                   "id": "OUR_SERVICE_4_ID",
-                  "title": "SECTION_2_ROW_2_TITLE"
+                  "title": "UI & UX Design"
                 }
               ]
             },
